1274fe5626cfedd0da6ed2c727582e19e8f1c773
ahinski <ahinski.inc@gmail.com>
2022-02-25 16:00:00 +0300

    Fixed-point Iteration

--- src/FixedPointIteration.java
+++ src/FixedPointIteration.java
@@ -2,7 +2,7 @@ public class FixedPointIteration {
     static double algorithm(double x_null, double eps) {
         double x_K = x_null;
         double x_KPlusOne = (Math.pow(x_K, 2) / 2) + 0.5 - (Math.exp(x_K) / 4);
-        int iterationsQuantity = 0;
+        int iterationsQuantity = 1;
         while(Math.abs(x_K - x_KPlusOne) > eps) {
             x_K = x_KPlusOne;
             x_KPlusOne = (Math.pow(x_K, 2) / 2) + 0.5 - (Math.exp(x_K) / 4);
